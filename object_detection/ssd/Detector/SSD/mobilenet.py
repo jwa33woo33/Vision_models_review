1da870ea6f51002cc769743ed0d6180b13e82b44
HONGS <kh4vv@virginia.edu>
2021-10-29 07:42:33 -0500

    torchvision ssd

---
+++ object_detection/ssd/Detector/SSD/mobilenet.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from mobilenetv2 import MobileNetV2, mobilenet_v2, __all__ as mv2_all
+from mobilenetv3 import MobileNetV3, mobilenet_v3_large, mobilenet_v3_small, __all__ as mv3_all
+
+__all__ = mv2_all + mv3_all
